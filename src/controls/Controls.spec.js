3c2458322f5c9fa3a783fd2535bd0fce84e92cce
joerlop <jj.erlich155@gmail.com>
2019-06-19 15:57:05 -0500

    All tests passing but 1. Don't get why

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Controls from "./Controls";
 import "@testing-library/react/cleanup-after-each";
 import "jest-dom/extend-expect";
 
 describe("<Controls />", () => {
   it("Displays lock and close buttons", () => {
-    const component = render(
-      <Controls
-        locked={false}
-        closed={false}
-      />
-    );
+    const component = render(<Controls locked={false} closed={false} />);
     const lock = component.getByText("Lock Gate");
     expect(lock).toHaveTextContent(/lock gate/i);
 
@@ -19,16 +14,34 @@ describe("<Controls />", () => {
     expect(close).toHaveTextContent(/close gate/i);
   });
   it("Displays unlock and open buttons", () => {
-    const component = render(
-      <Controls
-        locked={true}
-        closed={true}
-      />
-    );
+    const component = render(<Controls locked={true} closed={true} />);
     const unlock = component.getByText("Unlock Gate");
     expect(unlock).toHaveTextContent(/unlock gate/i);
 
     const open = component.getByText("Open Gate");
     expect(open).toHaveTextContent(/open gate/i);
+  });
+  it("Button text changes to open gate", () => {
+    let { getByText } = render(<Controls />);
+
+    let button = getByText(/close gate/i);
+
+    fireEvent.click(button);
+
+    getByText(/open gate/i);
+  });
+  it("Lock button is disabled", () => {
+    const { getByText } = render(<Controls locked={false} closed={false} />);
+
+    let button = getByText(/lock gate/i);
+
+    expect(button).toHaveAttribute("disabled");
+  });
+  it("Lock button is disabled", () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />);
+
+    let button = getByText(/open gate/i);
+
+    expect(button).toHaveAttribute("disabled");
   });
 });
