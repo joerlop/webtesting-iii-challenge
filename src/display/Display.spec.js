e1c521794e2991c1879888c99f9facd58258eb42
joerlop <jj.erlich155@gmail.com>
2019-06-19 14:34:44 -0500

    First Display test passed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+import "@testing-library/react/cleanup-after-each"
+import 'jest-dom/extend-expect'
+
+ it("renders without crashing", () => {
+  render(<Display />);
+});
+
+describe("<Display />", () => {
+  it("Display unlocked and open", () => {
+    const component = render(<Display locked={false} closed={false} />);
+    const unlocked = component.getByText("Unlocked");
+    expect(unlocked).toHaveTextContent(/unlocked/i);
+
+    const open = component.getByText("Open");
+    expect(open).toHaveTextContent(/open/i);
+  });
+});
