b8c13a8ed22488b8274cc56d94622e478349bd79
joerlop <jj.erlich155@gmail.com>
2019-06-19 14:52:13 -0500

    First Controls test passing. Buttons are displayed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Controls from "./Controls";
+import "@testing-library/react/cleanup-after-each";
+import "jest-dom/extend-expect";
+
+describe("<Controls />", () => {
+  it("Displays lock and close buttons", () => {
+    const component = render(
+      <Controls
+        locked={false}
+        closed={false}
+      />
+    );
+    const lock = component.getByText("Lock Gate");
+    expect(lock).toHaveTextContent(/lock gate/i);
+
+    const close = component.getByText("Close Gate");
+    expect(close).toHaveTextContent(/close gate/i);
+  });
+});
