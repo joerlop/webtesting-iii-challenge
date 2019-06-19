f448e3240d8922ba664f58311993593b249b0b09
joerlop <jj.erlich155@gmail.com>
2019-06-19 14:28:45 -0500

    Basic Dashboard tests passing. All buttons showing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,23 @@
-// Test away
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+import "@testing-library/react/cleanup-after-each"
+
+describe("<Dashboard />", () => {
+  it("contains a lock gate button", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/lock gate/i);
+  });
+  it("contains a close gate button", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/close gate/i);
+  });
+  it("contains an open sign", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/open/i);
+  });
+  it("contains an unlocked sign", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/unlocked/i);
+  });
+});
