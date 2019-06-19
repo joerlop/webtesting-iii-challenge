6edd3c77c5cdb79b50bcf67dfaf57d62cf6b0d6e
joerlop <jj.erlich155@gmail.com>
2019-06-19 14:45:56 -0500

    Display classes tests passing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -18,11 +18,27 @@ describe("<Display />", () => {
     expect(open).toHaveTextContent(/open/i);
   });
   it("Displays locked and closed", () => {
-    const component = render(<Display locked={false} closed={false} />);
-    const locked = component.getByText(/locked/i);
+    const component = render(<Display locked={true} closed={true} />);
+    const locked = component.getByText("Locked");
     expect(locked).toHaveTextContent(/locked/i);
 
-    const closed = component.getByText(/locked/i);
-    expect(closed).toHaveTextContent(/locked/i);
+    const closed = component.getByText("Closed");
+    expect(closed).toHaveTextContent(/closed/i);
+  });
+  it("Uses red-led class", () => {
+    const component = render(<Display locked={true} closed={true} />);
+    const locked = component.getByText("Locked");
+    expect(locked).toHaveClass("red-led");
+
+    const closed = component.getByText("Closed");
+    expect(closed).toHaveClass("red-led");
+  });
+  it("Uses green-led class", () => {
+    const component = render(<Display locked={false} closed={false} />);
+    const unlocked = component.getByText("Unlocked");
+    expect(unlocked).toHaveClass("green-led");
+
+    const open = component.getByText("Open");
+    expect(open).toHaveClass("green-led");
   });
 });
